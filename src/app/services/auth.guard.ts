82083a2afee1c5a30ce127976205375d21d3368b
crycetruly <crycetruly@gmail.com>
2019-01-30 14:23:18 +0300

    Prevented users from accessing unauthorised routes

---
+++ src/app/services/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private afauth: AngularFireAuth, private router: Router) {
+
+  }
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    this.afauth.user.subscribe(user => {
+      if (user) {
+        return true;
+      }
+    });
+    this.router.navigateByUrl('login');
+    return false;
+  }
+}
